bcd5aa2a48226cd1181ec0769b8d8ce7df82913b
Leinad-Dev <55466788+Leinad-Dev@users.noreply.github.com>
2021-07-30 20:45:45 -0700

    Adding test files
    Adding test files. Initializing directory.

---
+++ Assets/MapGenerator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public string seed;
+    public bool useRandomSeed;
+    [Range(0,100)]
+    public int randomFillPercent;
+    int[,] map;
+
+
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
+    }
+
+
+
+    private void GenerateMap()
+    {
+        map = new int[width,height];
+        RandomFillMap();
+        
+        for (int i = 0; i < 5; i++)//smooth map
+        {
+            SmoothMap();
+        }
+    }
+
+    void RandomFillMap()
+    {
+        if(useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+            
+
+        System.Random rnd = new System.Random(seed.GetHashCode());
+
+        for (int x=0; x < width; x++)
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1) //set borders to walls
+                {
+                    map[x, y] = 1; //this sets map[] to a wall 
+                }
+                else
+                {
+                    map[x, y] = (rnd.Next(0, 100) < randomFillPercent) ? 1 : 0; //true is 1 (wall)
+                }
+            }        
+    }
+
+    void SmoothMap()
+    {
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+            {
+                int neighbourWallTiles = GetSurroudingWallCount(x, y);
+
+                if (neighbourWallTiles > 4)
+                {
+                    map[x, y] = 1; //set this tile to a wall
+                }  
+                else if (neighbourWallTiles < 4)
+                {
+                    map[x, y] = 0;
+                }
+                    
+            }
+    }
+
+    int GetSurroudingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+        for (int neighbourX = gridX -1; neighbourX <= gridX + 1; neighbourX++)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+            {
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)//[1] make sure you're inside of the map
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)//not looking at original tile passed into this function
+                    {
+                        wallCount += map[neighbourX, neighbourY]; //if map[] is a wall then map[x,y] == 1. Else map[x,y] == 0.
+                    }
+                }
+
+                else
+                {
+                    wallCount++; //this will encourage the growth of walls around the edge of the map
+                }
+            }
+        }
+
+        return wallCount;
+    }
+
+
+    void OnDrawGizmos()
+    {
+        if (map != null)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                    Vector3 pos = new Vector3(-width/2 + x + .5f, 0, -height/2 + y + .5f);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+    }
+
+}
---
+++ Assets/MeshGenerator.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour
+{
+    public SquareGrid squareGrid;
+    public void GenerateMesh(int[,]map, float squareSize)
+    {
+        squareGrid = new SquareGrid(map, squareSize);
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Draw cube visualization for Nodes and ControlNodes
+        if (squareGrid != null)
+        {
+            for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+            {
+                for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+                {
+                    Gizmos.color = (squareGrid.squares[x, y].topLeft.active) ? Color.black : Color.white; //true = black, false = white
+                    Gizmos.DrawCube(squareGrid.squares[x, y].topLeft.position, Vector3.one * .4f);
+
+                    Gizmos.color = (squareGrid.squares[x, y].topRight.active) ? Color.black : Color.white; //true = black, false = white
+                    Gizmos.DrawCube(squareGrid.squares[x, y].topRight.position, Vector3.one * .4f);
+
+                    Gizmos.color = (squareGrid.squares[x, y].bottomLeft.active) ? Color.black : Color.white; //true = black, false = white
+                    Gizmos.DrawCube(squareGrid.squares[x, y].bottomLeft.position, Vector3.one * .4f);
+
+                    Gizmos.color = (squareGrid.squares[x, y].bottomRight.active) ? Color.black : Color.white; //true = black, false = white
+                    Gizmos.DrawCube(squareGrid.squares[x, y].bottomRight.position, Vector3.one * .4f);
+
+
+                    Gizmos.color = Color.grey;
+                    Gizmos.DrawCube(squareGrid.squares[x, y].centerTop.position, Vector3.one * .15f);
+                    Gizmos.DrawCube(squareGrid.squares[x, y].centerRight.position, Vector3.one * .15f);
+                    Gizmos.DrawCube(squareGrid.squares[x, y].centerBottom.position, Vector3.one * .15f);
+                    Gizmos.DrawCube(squareGrid.squares[x, y].centerLeft.position, Vector3.one * .15f);
+                    
+                }
+            }
+        }
+    }
+
+    public class SquareGrid
+    {
+        public Square[,] squares;
+        public SquareGrid(int[,] map,float squareSize)
+        {
+            int nodeCountX = map.GetLength(0);
+            int nodeCountY = map.GetLength(1);
+            float mapWidth = nodeCountX * squareSize;
+            float mapHeight = nodeCountY * squareSize;
+
+            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+
+            for (int x=0; x < nodeCountX; x++) //create a grid of control nodes
+            {
+                for(int y =0; y< nodeCountY; y++)
+                {
+                    Vector3 pos = new Vector3(-mapWidth / 2 + x * squareSize + squareSize / 2, 0, -mapHeight / 2 + y * squareSize + squareSize / 2);
+                    controlNodes[x, y] = new ControlNode(pos, map[x, y] == 1, squareSize);// ==1 checks for whether it's a wall
+                }
+            }
+
+
+            squares = new Square[nodeCountX - 1, nodeCountY - 1];
+            for (int x = 0; x < nodeCountX-1; x++) //create a grid squares out of controls nodes
+            {
+                for (int y = 0; y < nodeCountY-1; y++)
+                {
+                    //x   = left
+                    //x+1 = right
+                    //y   = bottom
+                    //y+1 = top
+
+                    // [tL]----------[tR]
+                    //   |            |
+                    //   |            |
+                    //   |            |
+                    //   |            |
+                    // [bL]----------[bR]
+
+                    //[ControlNode] topLeft = x, y+1
+                    //[ControlNode] topRight = x+1, y+1
+                    //[ControlNode] bottomRight = x+1, y
+                    //[ControlNode] bottomLeft = x, y
+
+                    squares[x, y] = new Square(controlNodes[x, y+1], controlNodes[x+1, y+1], controlNodes[x+1, y], controlNodes[x, y+1]);
+                        
+                }
+            }
+        }
+    }
+    public class Square
+    {
+        public ControlNode topLeft, topRight, bottomRight, bottomLeft;
+        public Node centerTop, centerRight, centerBottom, centerLeft;
+
+        public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
+        {
+
+            //       (cT)
+            // [tL]----*----[tR]
+            //  -            -  
+            //  -            -  
+            //  *(cL)        *(cR)  
+            //  -            -  
+            //  -            -  
+            // [bL]----*----[bR]
+            //       (cB)
+
+            topLeft = _topLeft;
+            topRight = _topRight; 
+            bottomRight = _bottomRight;
+            bottomLeft = _bottomLeft;
+
+            centerTop = topLeft.right;
+            centerRight = bottomRight.above;
+            centerBottom = bottomLeft.right;
+            centerLeft = bottomLeft.above;
+
+        }
+    }
+    public class Node
+    {
+        public Vector3 position;
+        public int vertexIndex = -1; //set to default -1 since we don't know what it's going to be
+
+        public Node(Vector3 _pos) //[1] base contructor
+        {
+            position = _pos;
+        }
+    }
+
+
+    public class ControlNode : Node //inherites from Node
+    {
+        public bool active;
+        public Node above, right;
+        public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos) //[1] _pos is set from the base construction
+        {
+            active = _active;
+            above = new Node(position + Vector3.forward * squareSize / 2f);
+            right = new Node(position + Vector3.right * squareSize / 2f);
+
+        }
+    }
+}
+
